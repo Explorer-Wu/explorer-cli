573150c7eccbfac6b98490a977b076940fee2244
探索者 <wuwenhan@WuWhs-MacBook-Pro.local>
2019-11-26 21:44:13 +0800

    初稿

---
+++ test/test-generate.js
@@ -0,0 +1,5 @@
+const path=require('path')
+const userHome=require('user-home')
+const generate=require('../../lib/generate-proj')
+const tmpPath=path.join(userHome,'.explorer-templates','webpack-cli-react-pc')
+generate(tmpPath)
